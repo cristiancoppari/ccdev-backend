ffd7bfb918db98bf4d4cd60e784d96b69e7f2b1b
Cristian <cristian.coppari@gmail.com>
2023-04-15 16:30:29 -0300

    add csrf-csrf

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,26 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
 import * as cookieParser from 'cookie-parser';
-import * as csurf from 'csurf';
+import { doubleCsrf } from 'csrf-csrf';
+
+const { generateToken, doubleCsrfProtection } = doubleCsrf({
+    getSecret: () => 'mrpeanutbutterhouse',
+    cookieName: 'csrfToken',
+});
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
-    // app.use(helmet());
+    app.use(helmet());
     app.enableCors({
         origin: 'http://localhost:3000',
         credentials: true,
     });
     app.use(cookieParser());
-    app.use(csurf({ cookie: { sameSite: true } }));
+    app.use(doubleCsrfProtection);
 
     await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
+
+export { generateToken };
