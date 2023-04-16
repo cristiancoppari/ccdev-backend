6c3c8812b604ac6e976358dde60a556a60784c64
Cristian <cristian.coppari@gmail.com>
2023-04-16 13:48:19 -0300

    rename env variables

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
 
     app.use(helmet());
     app.enableCors({
-        origin: process.env.CORS_PROD_ORIGIN || 'http://localhost:3000',
+        origin: process.env.CORS_ORIGIN,
         credentials: true,
     });
     app.use(cookieParser());
